bde61576a5e6e053d7d0f49eea4884951d9862fa
tisenor007 <77278112+tisenor007@users.noreply.github.com>
2021-03-03 18:44:46 -0400

    added things
    added very own first person character controller that jumps

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
     public GameObject Checkpointtext;
     public Vector3 SpawnPosition;
     //public Vector3 DefaultSpawnPos;
     public GameObject player;
     public AudioSource pickupsound;
-   
+    public Transform playerbody;
+    public float mousesensitivity = 100f;
+    public float xRotation = 0f;
+    public float YRotation = 0f;
+    public float Jumpheight;
+    private float Yposition;
+
     public AudioSource Checkpoint;
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         SpawnPosition = player.transform.position;
         //DefaultSpawnPos = new Vector3(0.09f, 0.78f, -18.6f);
         Debug.Log(SpawnPosition);
@@ -58,6 +66,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //move
+        float Xaxis = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float Yaxis = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        //rotate body, gives camera child rotate effect
+        float mouseX = Input.GetAxis("Mouse X") * mousesensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mousesensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        YRotation += mouseX;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Yposition = Jumpheight;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            Yposition = 0f;
+        }
+
+        gameObject.transform.Translate(Xaxis, Yposition, Yaxis);
+        transform.localRotation = Quaternion.Euler(xRotation, YRotation, 0f);
+        playerbody.Rotate(Vector3.up * mouseX);
     }
 }
